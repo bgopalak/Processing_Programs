e4f7bda8b8bedaec6a564d62cffa0012836352e6
bgopalak@gmail.com <Stinkydog143>
2018-01-15 01:58:50 -0500

    Experimenting with while loop

--- WhileLoop/WhileLoop.pde
+++ WhileLoop/WhileLoop.pde
@@ -1,8 +1,73 @@
-void setup(){
+Circle c1 = new Circle(200,200,20);
+Circle c2 = new Circle(0,300,100);
 
+Circle[] C =  new Circle [100];
 
+int i = 0;
+
+void setup() {
+  size(400,400);
+  while (i < 100){
+    C[i]= new Circle(random(10,350),random(10,350),25);
+    i++;
+  }
+  c1.speed(1);
+  c2.speed(2);
 }
 
+// Just simple movements
 void draw(){
+  background(103);
+  //c1.change(0,200,100);
+  c1.show();
+  //c2.change(0,300,100);
 
+  c2.show();
+  c1.move();
+  c2.move();
+
+  i = 0;
+  while(i < 100)
+  {
+    C[i].show();
+    i++;
+  }
+}
+
+
+// Circle Class. All definitions go here
+class Circle {
+float x,y;
+float diameter;
+float speed;
+
+Circle(float xPos, float yPos, float size ){
+  x= xPos;
+  y= yPos;
+  diameter = size;
+  speed = 0.1;
+  }
+void change(float xPos, float yPos, float size ){
+  x= xPos;
+  y= yPos;
+  diameter = size;
+  //speed = 0;
+  }
+
+void show(){
+  //fill(255,0,0);
+  fill(random(0,255),random(0,255),random(0,255));
+  noStroke();
+  ellipse(x,y,diameter/2,diameter/2);
+  }
+
+void speed(float sp){
+  speed = sp;
+  }
+
+void move() {
+  x = x+speed;
+  if(x >= 400+diameter)
+    x=0;
+}
 }
--- WhileLoop/build/source/WhileLoop.java
+++ WhileLoop/build/source/WhileLoop.java
@@ -14,14 +14,80 @@ import java.io.IOException;
 
 public class WhileLoop extends PApplet {
 
-public void setup(){
+Circle c1 = new Circle(200,200,20);
+Circle c2 = new Circle(0,300,100);
 
+Circle[] C =  new Circle [100];
 
+int i = 0;
+
+public void setup() {
+  
+  while (i < 100){
+    C[i]= new Circle(random(10,350),random(10,350),25);
+    i++;
+  }
+  c1.speed(1);
+  c2.speed(2);
 }
 
+// Just simple movements
 public void draw(){
+  background(103);
+  //c1.change(0,200,100);
+  c1.show();
+  //c2.change(0,300,100);
 
+  c2.show();
+  c1.move();
+  c2.move();
+
+  i = 0;
+  while(i < 100)
+  {
+    C[i].show();
+    i++;
+  }
 }
+
+
+// Circle Class. All definitions go here
+class Circle {
+float x,y;
+float diameter;
+float speed;
+
+Circle(float xPos, float yPos, float size ){
+  x= xPos;
+  y= yPos;
+  diameter = size;
+  speed = 0.1f;
+  }
+public void change(float xPos, float yPos, float size ){
+  x= xPos;
+  y= yPos;
+  diameter = size;
+  //speed = 0;
+  }
+
+public void show(){
+  //fill(255,0,0);
+  fill(random(0,255),random(0,255),random(0,255));
+  noStroke();
+  ellipse(x,y,diameter/2,diameter/2);
+  }
+
+public void speed(float sp){
+  speed = sp;
+  }
+
+public void move() {
+  x = x+speed;
+  if(x >= 400+diameter)
+    x=0;
+}
+}
+  public void settings() {  size(400,400); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "WhileLoop" };
     if (passedArgs != null) {
